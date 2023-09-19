ccd329cb0601b06bf491247036e62e3703af2a62
nika <nikamunja31415@gmail.com>
2023-09-19 08:42:41 +0400

    add lecturer code

---
+++ data/data.js
@@ -0,0 +1,19 @@
+
+export const obj = [
+    {
+      name: "nika",
+      age: 21,
+    },
+    {
+      name: "dato",
+      age: 24,
+    },
+    {
+      name: "luka",
+      age: 17,
+    },
+    {
+      name: "saba",
+      age: 14,
+    },
+  ];
